bedce6ebfddfa61ce62e65cb2e6be9ebc76dcf74
Clayton Carlson <claytonrc97@gmail.com>
2023-02-02 09:41:01 -0800

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const btn = document.createElement('button');
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  function addToDo(task) {
+    const li = document.createElement('li');
+    //const btn = document.createElement('button');
+    li.textContent = `${task}`;
+    btn.textContent = 'delete'
+    document.querySelector('ul').appendChild(li);
+    document.querySelector('li').appendChild(btn);
+  }
+  document.querySelector('form').addEventListener('submit', (e) => 
+  {e.preventDefault();
+  addToDo(e.target.newTaskDesc.value);
+
+  btn.addEventListener('click', deleteTask)
+  function deleteTask (e) {
+e.target.parentElement.remove();
+  }
+});
+
+//function addToDo(task) {
+ // const li = document.createElement('li');
+  //li.innertext = e.target.newTaskDesc.value;
+  //document.querySelector('ul').appendChild(li);
 });
